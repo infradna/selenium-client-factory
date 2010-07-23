f00c2fb6e92b73951c4c1b7ff600d1c180224f21
Kohsuke Kawaguchi <kk@kohsuke.org>
2010-07-23 15:15:01 -0700

    improved the test.

--- selenium-embedded-rc-driver/src/test/java/org/seleniumhq/selenium/client/logging/LoggingSeleniumTest.java
+++ selenium-embedded-rc-driver/src/test/java/org/seleniumhq/selenium/client/logging/LoggingSeleniumTest.java
@@ -4,16 +4,52 @@ import com.thoughtworks.selenium.Selenium;
 import junit.framework.TestCase;
 import org.seleniumhq.selenium.client.factory.SeleniumFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 /**
  * @author Kohsuke Kawaguchi
  */
 public class LoggingSeleniumTest extends TestCase {
+    private List<LogRecord> logs = new ArrayList<LogRecord>();
+
     public void test1() {
         Selenium s = SeleniumFactory.create("log:embedded-rc:*firefox", "http://www.google.com/");
-        assertTrue(s instanceof LoggingSelenium);
+
+        LoggingSelenium ls = (LoggingSelenium)s;
+        Logger l = Logger.getAnonymousLogger();
+        ls.setLogger(l);
+        l.addHandler(new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                logs.add(record);
+            }
+
+            @Override
+            public void flush() {
+            }
+
+            @Override
+            public void close() throws SecurityException {
+            }
+        });
+
         s.start();
         s.open("http://www.google.com/");
         assertEquals("Google",s.getTitle());
         s.stop();
+
+        verifyLog();
+    }
+
+    private void verifyLog() {
+        for (LogRecord log : logs) {
+            if (log.getMessage().contains("open(\"http://www.google.com/\")"))
+                return; // found it
+        }
+        fail("Log not recorded");
     }
 }
