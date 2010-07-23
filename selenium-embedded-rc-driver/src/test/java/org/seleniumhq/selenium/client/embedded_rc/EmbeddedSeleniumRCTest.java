3080444572fe4674375b8de4c92d49ea69e66c7d
Kohsuke Kawaguchi <kk@kohsuke.org>
2010-07-23 14:54:06 -0700

    added a test case

---
+++ selenium-embedded-rc-driver/src/test/java/org/seleniumhq/selenium/client/embedded_rc/EmbeddedSeleniumRCTest.java
@@ -0,0 +1,18 @@
+package org.seleniumhq.selenium.client.embedded_rc;
+
+import com.thoughtworks.selenium.Selenium;
+import junit.framework.TestCase;
+import org.seleniumhq.selenium.client.factory.SeleniumFactory;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class EmbeddedSeleniumRCTest extends TestCase {
+    public void test1() {
+        Selenium s = SeleniumFactory.create("embedded-rc:*firefox", "http://www.google.com/");
+        s.start();
+        s.open("http://www.google.com/");
+        assertEquals("Google",s.getTitle());
+        s.stop();
+    }
+}
