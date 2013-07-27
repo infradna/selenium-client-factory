e09d3830c001f65f911a45aa7c9edd4ef995995f
Ross Rowe <piaross@gmail.com>
2013-07-27 16:41:17 +1000

    Updated createWebDrivers method to use a thread pool to improve performance

--- sauce-ondemand-driver/src/main/java/com/saucelabs/sauce_ondemand/driver/SauceOnDemandSPIImpl.java
+++ sauce-ondemand-driver/src/main/java/com/saucelabs/sauce_ondemand/driver/SauceOnDemandSPIImpl.java
@@ -43,6 +43,7 @@ import java.net.URL;
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.concurrent.*;
 
 /**
  * {@link SeleniumFactorySPI} that talks to Sauce OnDemand.
@@ -284,15 +285,24 @@ public class SauceOnDemandSPIImpl extends SeleniumFactorySPI {
         return v;
     }
 
+    /**
+     * Creates a list of WebDriver instances based on the contents of a SAUCE_ONDEMAND_BROWSERS environment variable (typically set
+     * by the Sauce Jenkins plugin).
+     *
+     * @param seleniumFactory
+     * @param browserURL
+     * @return
+     */
     @Override
-    public List<WebDriver> createWebDrivers(SeleniumFactory seleniumFactory, String browserURL) {
+    public List<WebDriver> createWebDrivers(SeleniumFactory seleniumFactory, final String browserURL) {
 
         List<WebDriver> webDrivers = new ArrayList<WebDriver>();
         String browserJson = readPropertyOrEnv("SAUCE_ONDEMAND_BROWSERS", null);
         if (browserJson == null) {
             throw new IllegalArgumentException("Unable to find SAUCE_ONDEMAND_BROWSERS environment variable");
         }
-        //parse JSON
+        //parse JSON and extract the browser urls, so that we know how many threads to schedule
+        List<String> browsers = new ArrayList<String>();
         try {
             JSONArray array = new JSONArray(new JSONTokener(browserJson));
             for (int i = 0; i < array.length(); i++) {
@@ -304,11 +314,34 @@ public class SauceOnDemandSPIImpl extends SeleniumFactorySPI {
                 uri = uri.substring(SCHEME.length());
                 if (!uri.startsWith("?"))
                     throw new IllegalArgumentException("Missing '?':" + uri);
-                webDrivers.add(createWebDriver(browserURL, null, uri));
+                browsers.add(uri);
             }
         } catch (JSONException e) {
             throw new IllegalArgumentException("Error parsing JSON", e);
         }
+
+        //create a fixed thread pool for the number of browser
+        ExecutorService service = Executors.newFixedThreadPool(browsers.size());
+        List<Callable<WebDriver>> callables = new ArrayList<Callable<WebDriver>>();
+        for (final String browser : browsers) {
+            callables.add(new Callable<WebDriver>() {
+                public WebDriver call() throws Exception {
+                    return createWebDriver(browserURL, null, browser);
+                }
+            });
+        }
+        //invoke all the callables, and wait for each thread to return
+        try {
+            List<Future<WebDriver>> futures = service.invokeAll(callables);
+            for (Future<WebDriver> future : futures) {
+                webDrivers.add(future.get());
+            }
+        } catch (InterruptedException e) {
+            throw new IllegalArgumentException("Error retrieving webdriver", e);
+        } catch (ExecutionException e) {
+            throw new IllegalArgumentException("Error retrieving webdriver", e);
+        }
+        service.shutdown();
 
         return Collections.unmodifiableList(webDrivers);
     }
